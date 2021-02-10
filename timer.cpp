2b9be7e8b06fac0d2ec0565cbef2af953e60298e
Aseem90 <33744074+Aseem90@users.noreply.github.com>
2021-02-10 11:14:56 +0530

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -6,38 +6,7 @@
 * Date:             10/02/2021
 ******************************************************************************/
 
-#include <iostream>
-#include <functional>
-#include <thread>
 #include <chrono>
-#include <atomic>
-#include <condition_variable>
-
-class Timer
-{
-public:
-    Timer(uint64_t timeMs, const std::function<void()>& callback, bool autoReset);
-
-    ~Timer();
-
-    void Start();
-
-    void Stop();
-
-    void Restart(uint64_t timeMs = 0UL);
-
-    void Dispose();
-
-private:
-    std::thread m_TimerThread;
-    void TimerThreadFunc();
-    std::atomic_bool m_Active{false};
-    bool m_AutoReset = false;
-    std::function<void()> m_Callback = nullptr;
-    uint64_t m_TimeInterval = 0U;
-    std::condition_variable m_CondVar;
-    std::mutex m_MutexForCV;
-};
 
 Timer::Timer(uint64_t timeMs, const std::function<void()>& callback, bool autoReset)
 {
@@ -76,7 +45,6 @@ void Timer::TimerThreadFunc()
 
 void Timer::Start()
 {
-    std::cout << "Starting Timer" << "\n";
     if (!m_Callback)
     {
         return;
@@ -94,7 +62,6 @@ void Timer::Start()
 
 void Timer::Restart(uint64_t timeMs)
 {
-    std::cout << "Restarting Timer" << "\n";
     if (timeMs != 0UL)
     {
         m_TimeInterval = timeMs;
@@ -105,7 +72,6 @@ void Timer::Restart(uint64_t timeMs)
 
 void Timer::Stop()
 {
-    std::cout << "Stopping Timer" << "\n";
     {
         std::lock_guard<std::mutex> lg(m_MutexForCV);
         m_Active = false;
@@ -119,26 +85,4 @@ void Timer::Dispose()
     this->Stop();
     m_TimeInterval = 0UL;
     m_Callback = nullptr;
-}
-
-void WhenTimerExpire()
-{
-    std::cout << "myTimer expired!! " << "\n";
-}
-
-int main()
-{
-    Timer myTimer(3000UL, &WhenTimerExpire, true);
-    myTimer.Start();
-
-    // std::this_thread::sleep_for(std::chrono::seconds(121));
-
-    // Test2: Stop the timer after 7 secs but main thread should still be running. 
-    std::this_thread::sleep_for(std::chrono::seconds(13));
-    myTimer.Stop();
-    myTimer.Restart(5000UL);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(25005));
-    std::cout << "Main exiting now..." << "\n";
-    return 0;
 }
