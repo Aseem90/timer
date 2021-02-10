68e56bc263bb8cced929105644fb5f3c2e80736c
Aseem90 <33744074+Aseem90@users.noreply.github.com>
2021-02-10 11:11:32 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
-/***************************************************************
+/******************************************************************************
+* File Name:        main.cpp
+* Description:      Driver code to test the Timer
+* Notes:            
+* Author:           Aseem Tiwari
+* Date:             10/02/2021
+******************************************************************************/
 
+#include "timer.hpp"
 
+// Event handler
+void WhenTimerExpire()
+{
+    std::cout << "myTimer expired!! " << "\n";
+}
 
-**************************************************************/
+// Driver code to test timer
+int main()
+{
+    Timer myTimer(3000UL, &WhenTimerExpire, true);
+    myTimer.Start();
+
+    // std::this_thread::sleep_for(std::chrono::seconds(121));
+
+    // Test2: Stop the timer after 7 secs but main thread should still be running. 
+    std::this_thread::sleep_for(std::chrono::seconds(13));
+    myTimer.Stop();
+    myTimer.Restart(5000UL);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(25005));
+    return 0;
+}
