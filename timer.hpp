9a11d2095ea1657299f860d4bc4206b855b4a3f1
Aseem90 <33744074+Aseem90@users.noreply.github.com>
2021-02-10 11:14:50 +0530

    Update timer.hpp

--- timer.hpp
+++ timer.hpp
@@ -1,5 +1,37 @@
-/**********************************************************************
-*
+/******************************************************************************
+* File Name:        timer.hpp
+* Description:      
+* Notes:            
+* Author:           Aseem Tiwari
+* Date:             10/02/2021
+******************************************************************************/
 
+#include <condition_variable>
+#include <functional>
+#include <thread>
 
-***********************************************************************/
+class Timer final
+{
+public:
+    Timer(uint64_t timeMs, const std::function<void()>& callback, bool autoReset);
+
+    ~Timer();
+
+    void Start();
+
+    void Stop();
+
+    void Restart(uint64_t timeMs = 0UL);
+
+    void Dispose();
+
+private:
+    std::thread m_TimerThread;
+    void TimerThreadFunc();
+    bool m_Active = false;
+    bool m_AutoReset = false;
+    std::function<void()> m_Callback = nullptr;
+    uint64_t m_TimeInterval = 0U;
+    std::condition_variable m_CondVar;
+    std::mutex m_MutexForCV;
+};
